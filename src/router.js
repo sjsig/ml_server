8db1d46c8d5a9ada7b925e3ac2770b7c4531f2c4
sjsig <steviejoesig49@gmail.com>
2020-05-25 19:07:28 -0400

    Add logged in requirement to create lease route

--- src/router.js
+++ src/router.js
@@ -15,53 +15,51 @@ router.get("/", (req, res) => {
 });
 
 // USER CONTROLLER
-router.route("/users/:userId")
-  .get(UserController.getUser)
-  .delete(UserController.deleteUser);
+router.route("/users/:userId").get(UserController.getUser).delete(UserController.deleteUser);
 
 router.post("/auth/signin", UserController.signin);
 router.post("/auth/signup", UserController.signup);
 
 // PROPERTY CONTROLLER
-router.route("/property/:propertyId")
+router
+  .route("/property/:propertyId")
   .get(isAuthorized, PropertyController.getProperty)
   .delete(isAuthorized, PropertyController.deleteProperty)
   .put(isAuthorized, PropertyController.updateProperty);
 
-router.route("/property")
-.get(isLoggedIn, PropertyController.getAllProperties)
-.post(isLoggedIn, PropertyController.createProperty);
+router
+  .route("/property")
+  .get(isLoggedIn, PropertyController.getAllProperties)
+  .post(isLoggedIn, PropertyController.createProperty);
 
-router.route("/listings")
-.get(PropertyController.getVacantUnits)
+router.route("/listings").get(PropertyController.getVacantUnits);
 
 // UNIT CONTROLLER
-router.route("/property/:propertyId/unit/:unitId")
-.get(UnitController.getUnit)
-.delete(UnitController.deleteUnit)
-.put(UnitController.updateUnit)
+router
+  .route("/property/:propertyId/unit/:unitId")
+  .get(UnitController.getUnit)
+  .delete(UnitController.deleteUnit)
+  .put(UnitController.updateUnit);
 
-router.route("/property/:propertyId/unit")
-.get(UnitController.getAllUnitsByProperty)
-.post(UnitController.createUnit);
+router.route("/property/:propertyId/unit").get(UnitController.getAllUnitsByProperty).post(UnitController.createUnit);
 
 // LEASE CONTROLLER
-router.route("/unit/:unitId/lease/:leaseId")
-.get(LeaseController.getLease)
-.delete(LeaseController.deleteLease)
-.put(LeaseController.updateLease)
+router
+  .route("/unit/:unitId/lease/:leaseId")
+  .get(LeaseController.getLease)
+  .delete(LeaseController.deleteLease)
+  .put(LeaseController.updateLease);
 
-router.route("/unit/:unitId/lease")
-.post(LeaseController.createLease);
+router.route("/unit/:unitId/lease").post(isLoggedIn, LeaseController.createLease);
 
 // RATING CONTROLLER
-router.route("/user/:userId/rating")
-.post(isLoggedIn, RatingController.createRating)
-.get(RatingController.getRatingByUser);
+router
+  .route("/user/:userId/rating")
+  .post(isLoggedIn, RatingController.createRating)
+  .get(RatingController.getRatingByUser);
 
 // TRANSACTION CONTROLLER
 
-router.route('/user/:userId/transaction')
-  .get(isAuthorized, TransactionController.getUserTransactionHistory)
+router.route("/user/:userId/transaction").get(isAuthorized, TransactionController.getUserTransactionHistory);
 
 export default router;
