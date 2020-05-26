27242934b5695383371cbb68f29101b4aa5975b6
Theodore <theodorewahle@gmail.com>
2020-05-26 15:15:50 -0400

    removed comment block

--- src/server.js
+++ src/server.js
@@ -25,36 +25,7 @@ const app = express();
 global.connection = mysql.createConnection(process.env.DB_URL);
 global.connection.connect();
 
-/*
-function handleDisconnect() {
-  console.log("reconnecting to db")
-  global.connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  }); // Recreate the connection, since
-                                                  // the old one cannot be reused.
 
-  global.connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  global.connection.on('error', function(err) {
-    console.log('db error', err.code);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      console.log("calling handle db")
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-       throw err;                                 // server variable configures this)
-    }});
-}
-
-handleDisconnect();
-*/
 // enable/disable cross origin resource sharing if necessary
 app.use(cors());
 
